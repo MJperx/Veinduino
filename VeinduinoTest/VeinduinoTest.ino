150c9c213c103c01ace229789f41315d4af35130
madar94 <madar94@gmail.com>
2017-03-26 23:29:44 +0200

    .ino update

--- VeinduinoTest/VeinduinoTest.ino
+++ VeinduinoTest/VeinduinoTest.ino
@@ -1,14 +1,28 @@
 #include <Arduino.h>
 #include "Veinduino.h"
 
-Veinduino veinduino;
+Veinduino* veinduino;
+char user_input;
 
 void setup() {
-    Serial.begin(115200);
-
-    veinduino = new Veinduino(2,3,4,5,6,7,A0,A1);
+    veinduino = new Veinduino(9,8,7,A0,A1,2,3);
 }
 
 void loop() {
-    veinduino.goMotor(1000);
+  while(Serial.available()){
+    user_input = Serial.read();
+    if (user_input == '1') {
+      veinduino->goMotor(3000);
+    } else if (user_input == '2') {
+      veinduino->goMotorBack(3000);
+    } else if (user_input == '3'){
+      veinduino->beginPull();
+    } else {
+      Serial.println("Error");
+    }
+    veinduino->resetMotor();
+  }
+  if (digitalRead(veinduino->_rev) == LOW){
+    veinduino->goMotorBack(3000);
+  }
 }
