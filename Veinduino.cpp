95abc8a7093f501f18896da76b0381e3fc2bfd09
Apró Anikó <apro.ancsa@gmail.com>
2017-04-09 10:14:37 +0200

    Updated Veinduino class

--- Veinduino.cpp
+++ Veinduino.cpp
@@ -1,50 +1,45 @@
-//
-// Created by MJ on 2017. 03. 18..
-//
-
 #include "Veinduino.h"
 
-Veinduino::Veinduino(uint8_t step, uint8_t dir, uint8_t pump,
-                     uint8_t pdsck, uint8_t dout, uint8_t interruptpin, uint8_t reverse) {
+Veinduino::Veinduino(uint8_t step, uint8_t dir, uint8_t ena,
+                     uint8_t pump, uint8_t interruptpin,
+                     uint8_t reverse, uint8_t pressure) {
 
     this->_step = step;
     this->_dir = dir;
+    this->_enable = ena;
     this->_pump = pump;
-    this->_pdsck = pdsck;
-    this->_dout = dout;
     this->_inter = interruptpin;
     this->_rev = reverse;
+    this->_pressure = pressure;
+
+    Serial.begin(115200);
+
     pinMode(_step, OUTPUT);
     pinMode(_dir,  OUTPUT);
+    //pinMode(_enable, OUTPUT);
     pinMode(_pump, OUTPUT);
-    pinMode(_pdsck, OUTPUT);
-    pinMode(_dout, INPUT);
     pinMode(_inter, INPUT);
     pinMode(_rev, INPUT_PULLUP);
-    ScaleSetGain(128);
-    ScaleSetScale(408.5f);
-    ScaleTare();
-    Serial.begin(115200);
+    pinMode(_pressure, INPUT);
+    digitalWrite(_pump,LOW);
+    digitalWrite(_enable,HIGH);
 }
 
-
 void Veinduino::beginPull() {
+  digitalWrite(_enable, LOW);
   digitalWrite(_dir, HIGH);
   for(int x= 1; x < 3000; x++) {
       digitalWrite(_step,HIGH);
       delay(1);
       digitalWrite(_step,LOW);
       delay(1);
-      Serial.println(ScaleGetUnits(10));
-      ScalePowerDown();
-      delayMicroseconds(10);
-      ScalePowerUp();
   }
   resetMotor();
 }
 
 void Veinduino::goMotor(uint16_t step) {
     digitalWrite(_dir, HIGH);
+    digitalWrite(_enable, LOW);
     for(int x= 1; x < step; x++) {
         digitalWrite(_step,HIGH);
         delay(1);
@@ -56,6 +51,7 @@ void Veinduino::goMotor(uint16_t step) {
 
 void Veinduino::goMotorBack(uint16_t step) {
     digitalWrite(_dir, LOW);
+    digitalWrite(_enable, LOW);
     unsigned long currentMillis = millis();
     for (int x = 1; x < step; x++) {
       if (currentMillis - previousPumpMillis >= 1) {
@@ -69,113 +65,15 @@ void Veinduino::goMotorBack(uint16_t step) {
 }
 
 void Veinduino::goPump(unsigned long time) {
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousPumpMillis >= time) {
-        previousPumpMillis = currentMillis;
-        if (pumpState == LOW) {
-            pumpState = HIGH;
-        } else {
-            pumpState = LOW;
-        }
-        digitalWrite(_pump, pumpState);
+    digitalWrite(_pump,HIGH);
+    for (int i = 0; i < time; i++){
+      Serial.println(analogRead(_pressure));
+      delay(1);
     }
+    digitalWrite(_pump,LOW);
 }
 
 void Veinduino::resetMotor() {
     digitalWrite(_step, LOW);
     digitalWrite(_dir, LOW);
-}
-
-void Veinduino::ScaleSetGain(uint8_t gain) {
-    switch (gain) {
-        case 128:
-            GAIN = 1;
-            break;
-        case 64:
-            GAIN = 3;
-            break;
-        case 32:
-            GAIN = 2;
-            break;
-    }
-    digitalWrite(_pdsck, LOW);
-    ScaleRead();
-}
-
-long Veinduino::ScaleRead() {
-    unsigned long value = 0;
-    uint8_t data[3] = { 0 };
-    uint8_t filler = 0x00;
-
-    data[2] = shiftIn(_dout, _pdsck, MSBFIRST);
-    data[1] = shiftIn(_dout, _pdsck, MSBFIRST);
-    data[0] = shiftIn(_dout, _pdsck, MSBFIRST);
-
-    for (uint8_t i = 0; i < GAIN; i++) {
-        digitalWrite(_pdsck, HIGH);
-        digitalWrite(_pdsck, LOW);
-    }
-
-    if (data[2] & 0x80) {
-        filler = 0xFF;
-    } else {
-        filler = 0x00;
-    }
-
-    value = ( static_cast<unsigned long>(filler) << 24
-              | static_cast<unsigned long>(data[2]) << 16
-              | static_cast<unsigned long>(data[1]) << 8
-              | static_cast<unsigned long>(data[0]) );
-
-    return static_cast<long>(value);
-}
-
-long Veinduino::ScaleReadAvarage(byte times) {
-    long sum = 0;
-    for (uint8_t i = 0; i < times; i++) {
-        sum += ScaleRead();
-    }
-    return sum / times;
-}
-
-bool Veinduino::ScaleIsReady() {
-    return digitalRead(_dout) == LOW;
-}
-
-double Veinduino::ScaleGetValue(byte times) {
-	return ScaleReadAvarage(times) - OFFSET;
-}
-
-float Veinduino::ScaleGetUnits(byte times) {
-	return ScaleGetValue(times) / SCALE;
-}
-
-void Veinduino::ScaleTare(byte times) {
-	double sum = ScaleReadAvarage(times);
-	ScaleSetOffset(sum);
-}
-
-void Veinduino::ScaleSetScale(float scale) {
-	SCALE = scale;
-}
-
-float Veinduino::ScaleGetScale() {
-	return SCALE;
-}
-
-void Veinduino::ScaleSetOffset(long offset) {
-	OFFSET = offset;
-}
-
-long Veinduino::ScaleGetOffset() {
-	return OFFSET;
-}
-
-void Veinduino::ScalePowerDown() {
-	digitalWrite(_pdsck, LOW);
-	digitalWrite(_pdsck, HIGH);
-}
-
-void Veinduino::ScalePowerUp() {
-	digitalWrite(_pdsck, LOW);
 }
--- Veinduino.h
+++ Veinduino.h
@@ -1,7 +1,3 @@
-//
-// Created by MJ on 2017. 03. 18..
-//
-
 #pragma once
 
 #include "Arduino.h"
@@ -10,31 +6,20 @@
 class Veinduino {
     public:
         Veinduino();
-        Veinduino(uint8_t step, uint8_t dir, uint8_t pump,
-                  uint8_t pdsck, uint8_t dout, uint8_t interruptpin, uint8_t reverse);
+        Veinduino(uint8_t step, uint8_t dir, uint8_t ena,
+                  uint8_t pump, uint8_t interruptpin,
+                  uint8_t reverse, uint8_t pressure);
 
-        uint8_t _rev;
+        uint8_t _rev, _inter, _pressure;
+        uint8_t  _pump, _step, _dir, _enable;
+        volatile int value = 0;
 
-        long ScaleRead();
-        long ScaleReadAvarage(byte times);
-        long ScaleGetOffset();
-        double ScaleGetValue(byte times = 1);
-    		float ScaleGetUnits(byte times = 1);
-        float ScaleGetScale();
-        bool ScaleIsReady();
-    		void ScaleTare(byte times = 10);
-    		void ScaleSetScale(float scale = 1.f);
-    		void ScaleSetOffset(long offset = 0);
-    		void ScalePowerDown();
-    		void ScalePowerUp();
-        void ScaleSetGain(uint8_t gain);
         void goMotor(uint16_t step);
         void goMotorBack(uint16_t step);
         void goPump(unsigned long time);
         void resetMotor();
         void beginPull();
     private:
-        uint8_t _step, _dir, _pump, _pdsck, _dout, _inter;
         uint8_t pumpState = LOW;
         bool forwardState = false;
         uint8_t GAIN;
